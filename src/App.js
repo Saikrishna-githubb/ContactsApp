4919ca011c53edb8dd81e7f85866ae7fd019622b
SaiKrishna <2022saikrishna@gmail.com>
2022-09-21 14:34:44 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ContactItem from "./components/ContactItem";
+
+import "./App.css";
+
+const initialContactsList = [
+  {
+    id: uuidv4(),
+    name: "Ram",
+    mobileNo: 9999988888,
+    isFavorite: false,
+  },
+  {
+    id: uuidv4(),
+    name: "Pavan",
+    mobileNo: 8888866666,
+    isFavorite: true,
+  },
+  {
+    id: uuidv4(),
+    name: "Nikhil",
+    mobileNo: 9999955555,
+    isFavorite: false,
+  },
+];
+
+class App extends Component {
+  state = {
+    contactsList: initialContactsList,
+    name: "",
+    mobileNo: "",
+  };
+
+  toggleIsFavorite = (id) => {
+    this.setState((prevState) => ({
+      contactsList: prevState.contactsList.map((eachContact) => {
+        if (id === eachContact.id) {
+          return { ...eachContact, isFavorite: !eachContact.isFavorite };
+        }
+        return eachContact;
+      }),
+    }));
+  };
+
+  onAddContact = (event) => {
+    event.preventDefault();
+    const { name, mobileNo } = this.state;
+    const newContact = {
+      id: uuidv4(),
+      name,
+      mobileNo,
+      isFavorite: false,
+    };
+
+    this.setState((prevState) => ({
+      contactsList: [...prevState.contactsList, newContact],
+      name: "",
+      mobileNo: "",
+    }));
+  };
+
+  onChangeMobileNo = (event) => {
+    this.setState({ mobileNo: event.target.value });
+  };
+
+  onChangeName = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  render() {
+    const { name, mobileNo, contactsList } = this.state;
+    return (
+      <div className="app-container">
+        <div className="responsive-container">
+          <h1 className="heading">Contacts</h1>
+          <form className="contact-form-container" onSubmit={this.onAddContact}>
+            <input
+              value={name}
+              onChange={this.onChangeName}
+              className="input"
+              placeholder="Name"
+            />
+            <input
+              className="input"
+              value={mobileNo}
+              onChange={this.onChangeMobileNo}
+              placeholder="Mobile Number"
+            />
+            <button type="submit" className="button">
+              Add Contact
+            </button>
+          </form>
+          <ul className="contacts-table">
+            <li className="table-header">
+              <p className="table-header-cell name-column">Name</p>
+              <hr className="separator" />
+              <p className="table-header-cell">Mobile Number</p>
+            </li>
+            {contactsList.map((eachContact) => (
+              <ContactItem
+                key={eachContact.id}
+                contactDetails={eachContact}
+                toggleIsFavorite={this.toggleIsFavorite}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
